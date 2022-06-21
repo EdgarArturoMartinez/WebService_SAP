824363bb058dfba7fb2ae0e10206896d54716b00
Edgar Arturo Martínez <edgar.arturo.martinez@gmail.com>
2022-06-21 14:15:15 -0500

    Web Service Code attempt for SAP
    Commit #1 WebService for SAP

---
+++ Consumir_SAP_WS/Conexion.vb
@@ -0,0 +1,207 @@
+﻿Imports System.Data.Sql
+Imports System.Data.SqlClient
+Imports System.Data.SqlTypes
+
+
+Public Class Conexion
+
+    'Declarar Variables de Conexión
+    Public ds As New DataSet()
+    Public da As New SqlDataAdapter
+    Public dt As New DataTable
+    Public comando As SqlCommand
+    Public con As SqlConnection
+    Public col As DataColumn
+
+    'Declarar Función Conectar
+    Public Function Conectar() As SqlConnection
+        Dim cadena = ("Data Source=296imb01; Initial Catalog = db_Negocio_Inmobiliario; Persist Security Info=True;User ID=si2;Password=si2_VPinmobiliaria")
+        Dim con As New SqlConnection(cadena)
+        Try
+            con.Open()
+            Return con
+        Catch ex As Exception
+            Return con
+            con.Close()
+        End Try
+    End Function
+
+
+    'Función Para Realizar de Contratos a Sincronizar
+    Public Function NumContratos(Campo1 As String) As Boolean
+
+        Dim sql As String = " SELECT COUNT(*) FROM [dbo].[tbl_WF_Casual_Leasing_Condiciones_Negocio] " &
+                                " WHERE Condiciones_Validadas = 1 AND [Actualizado_SAP] = 0 " &
+                                " AND Id_GUID =  '" & Campo1 & "' "
+        con = Conectar()
+        comando = New SqlCommand(sql, con)
+        da = New SqlDataAdapter(comando)
+        ds = New DataSet()
+        da.Fill(ds)
+        dt = New DataTable()
+        dt = ds.Tables(0)
+
+        If (ds IsNot Nothing) Then
+            Return True
+        Else
+            Return False
+            con.Close()
+            ds.Dispose()
+        End If
+        con.Close()
+        ds.Dispose()
+
+    End Function
+
+
+
+    'Función Para Realizar Consulta en la Vista de la base de datos.
+    Public Function DatosSap(Campo1 As String) As Boolean
+
+        Dim sql As String = " SELECT TOP(1) Depben, Deprec, Contrato, Tipocon, str_Nit_Cedula, " &
+                                " Nombre_1, Nombre_2, Apellido_1, Apellido_2, " &
+                                " Dirección_Empresa, str_Telefono, str_Correo_Electronico, " &
+                                " FECHA_INICIAL, FECHA_FIN, Contraprestación, Ciudad_Cedula, " &
+                                " Tiponit, Burks, Pais, Region, Actualizado_SAP  " &
+                                " FROM Qry_WS_Casual_Leasing_F2 " &
+                                " WHERE Id_GUID =  '" & Campo1 & "' AND Actualizado_SAP = 0 "
+        con = Conectar()
+        comando = New SqlCommand(sql, con)
+        da = New SqlDataAdapter(comando)
+        ds = New DataSet()
+        da.Fill(ds)
+        dt = New DataTable()
+        dt = ds.Tables(0)
+
+        If (ds IsNot Nothing) Then
+            Return True
+        Else
+            Return False
+            con.Close()
+            ds.Dispose()
+        End If
+        con.Close()
+        ds.Dispose()
+
+    End Function
+
+
+
+    'Función para actualizar Dependencia 2032
+    Public Function Dep2032(campo1 As String, campo2 As String) As Boolean
+
+        Dim sql As String = " ALTER TABLE tbl_WF_Casual_Leasing_Condiciones_Negocio " &
+                            " DISABLE Trigger Validar_Condiciones_Casual_Leasing " &
+                            " UPDATE [dbo].[tbl_WF_Casual_Leasing_Condiciones_Negocio] " &
+                            " SET [Actualizado_SAP] = 1, " &
+                            " str_Console_Response = 'Dependencia 2032 Sin Recaudo para Sincronizar en SAP.  Fecha de Proceso = " & Day(Now) & "/" & Month(Now) & "/" & Year(Now) & "   Hora Proceso = " & Hour(Now) & ":" & Minute(Now) & " ' , " &
+                            " Validacion_Final = 1  WHERE Id_GUID = '" & campo1 & "' AND Id_Condiciones_Negocio = '" & campo2 & "' " &
+                            " ALTER TABLE tbl_WF_Casual_Leasing_Condiciones_Negocio ENABLE Trigger Validar_Condiciones_Casual_Leasing"
+        con = Conectar()
+        comando = New SqlCommand(sql, con)
+        Dim i As String = comando.ExecuteNonQuery()
+        con.Close()
+        If i > 0 Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
+
+
+    'Función para actualizar Dependencias Sin Filtro
+    Public Function ActualizarFinal(msgConsole As String, campo1 As String, campo2 As String, Param1 As String,
+                                    Param2 As String, Param3 As String, Param4 As String, Param5 As String, Param6 As String,
+                                    Param7 As String, Param8 As String, Param9 As String, Param10 As String, Param11 As String,
+                                    Param12 As String, Param13 As String, Param14 As String, Param15 As String, Param16 As String,
+                                    Param17 As String, Param18 As String, Param19 As String, Param20 As String) As Boolean
+
+        Dim sql As String = " ALTER TABLE tbl_WF_Casual_Leasing_Condiciones_Negocio " &
+                            " DISABLE Trigger Validar_Condiciones_Casual_Leasing " &
+                            " UPDATE [dbo].[tbl_WF_Casual_Leasing_Condiciones_Negocio] " &
+                            " SET [Actualizado_SAP] = 1, " &
+                            " str_Console_Response = '" & msgConsole & ".   Fecha de Proceso = " & Day(Now) & "/" & Month(Now) & "/" & Year(Now) & "   Hora Proceso = " & Hour(Now) & ":" & Minute(Now) &
+                            " SQL Ejecutada =    SELECT Depben, Deprec, Contrato, Tipocon, str_Nit_Cedula,Nombre_1, Nombre_2, Apellido_1, Apellido_2,Dirección_Empresa, str_Telefono, str_Correo_Electronico,FECHA_INICIAL, FECHA_FIN, Contraprestación, Ciudad_Cedula,Tiponit, Burks, Pais, Region, Actualizado_SAP FROM Qry_WS_Casual_Leasing_F2 WHERE Contrato = " & campo2 &
+                            "     Parametros Enviados:  " & Param1 & ", " & Param2 & ", " & Param3 & ", " & Param4 & ", " & Param5 & ", " & Param6 & ", " & Param7 & ", " & Param8 & ", " & Param9 & ", " & Param10 & ", " & Param11 & ", " & Param12 & ", " & Param13 & ", " & Param14 & ", " & Param15 & ", " & Param16 & ", " & Param17 & ", " & Param18 & ", " & Param19 & " ' , " &
+                            " Validacion_Final = 1  WHERE Id_GUID = '" & campo1 & "' AND Id_Condiciones_Negocio = '" & campo2 & "' " &
+                            " ALTER TABLE tbl_WF_Casual_Leasing_Condiciones_Negocio ENABLE Trigger Validar_Condiciones_Casual_Leasing"
+        con = Conectar()
+        comando = New SqlCommand(sql, con)
+        Dim i As String = comando.ExecuteNonQuery()
+        con.Close()
+        If i > 0 Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
+
+
+
+    'Función Para Realizar Consulta en la Vista de la base de datos.
+    Public Function DatosSuper(Campo1 As String) As Boolean
+
+        Dim sql As String = " SELECT TOP (1) tbl_M_Marcas.str_Descripción_Marca " &
+                                " FROM tbl_WF_Casual_Leasing_Condiciones_Negocio INNER JOIN " &
+                                " tbl_H_Areas_X_Inmueble_Detalle ON   " &
+                                " tbl_WF_Casual_Leasing_Condiciones_Negocio.Numero_Local = tbl_H_Areas_X_Inmueble_Detalle.lng_Id_Area_Detalle INNER JOIN " &
+                                " tbl_M_Inmueble_Detalle ON tbl_H_Areas_X_Inmueble_Detalle.str_Id_División = tbl_M_Inmueble_Detalle.str_División INNER JOIN " &
+                                " tbl_M_Marcas ON tbl_M_Inmueble_Detalle.lng_Id_Marca = tbl_M_Marcas.lng_Id_Marca " &
+                                " WHERE (tbl_WF_Casual_Leasing_Condiciones_Negocio.Id_GUID =  '" & Campo1 & "' ) "
+        con = Conectar()
+        comando = New SqlCommand(sql, con)
+        da = New SqlDataAdapter(comando)
+        ds = New DataSet()
+        da.Fill(ds)
+        dt = New DataTable()
+        dt = ds.Tables(0)
+
+        If (ds IsNot Nothing) Then
+            Return True
+        Else
+            Return False
+            con.Close()
+            ds.Dispose()
+        End If
+        con.Close()
+        ds.Dispose()
+
+    End Function
+
+
+    'Función Para Realizar Consulta en la Vista de la base de datos.
+    Public Function DatosSMX(Campo1 As String) As Boolean
+
+        Dim sql As String = " SELECT TOP (1) tbl_M_Marcas.str_Descripción_Marca " &
+                                " FROM tbl_WF_Casual_Leasing_Condiciones_Negocio INNER JOIN " &
+                                " tbl_H_Areas_X_Inmueble_Detalle ON   " &
+                                " tbl_WF_Casual_Leasing_Condiciones_Negocio.Numero_Local = tbl_H_Areas_X_Inmueble_Detalle.lng_Id_Area_Detalle INNER JOIN " &
+                                " tbl_M_Inmueble_Detalle ON tbl_H_Areas_X_Inmueble_Detalle.str_Id_División = tbl_M_Inmueble_Detalle.str_División INNER JOIN " &
+                                " tbl_M_Marcas ON tbl_M_Inmueble_Detalle.lng_Id_Marca = tbl_M_Marcas.lng_Id_Marca " &
+                                " WHERE (tbl_WF_Casual_Leasing_Condiciones_Negocio.Id_GUID =  '" & Campo1 & "' ) "
+        con = Conectar()
+        comando = New SqlCommand(sql, con)
+        da = New SqlDataAdapter(comando)
+        ds = New DataSet()
+        da.Fill(ds)
+        dt = New DataTable()
+        dt = ds.Tables(0)
+
+        If (ds IsNot Nothing) Then
+            Return True
+        Else
+            Return False
+            con.Close()
+            ds.Dispose()
+        End If
+        con.Close()
+        ds.Dispose()
+
+    End Function
+
+
+
+
+End Class
---
+++ Consumir_SAP_WS/Module1.vb
@@ -0,0 +1,224 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports Consumir_SAP_WS.sapdev
+Imports System.Data.Sql
+Imports System.Data.SqlClient
+Imports System.Data.SqlTypes
+Imports System.Xml
+Imports System.Net
+Imports System.Text
+Imports System.Net.Mime.MediaTypeNames
+Imports System.IO
+
+
+Module Module1
+    Public Connect1 As New Conexion()
+    Public Connect2 As New Conexion()
+    Public Connect3 As New Conexion()
+    Public Connect4 As New Conexion()
+    Public Connect5 As New Conexion()
+    Public Connect6 As New Conexion()
+
+    Sub Main()
+
+        Dim Guid As String
+
+        'Consulta de proyecto XML
+        Dim reader As XmlTextReader = New XmlTextReader("C:\Gabmer\SI²\Parametros Actividades.xml")
+        reader.WhitespaceHandling = WhitespaceHandling.None
+        reader.Read()
+        While Not reader.EOF
+            reader.Read()
+            If Not reader.IsStartElement() Then
+                Exit While
+            End If
+
+            Dim Casual As String
+
+            Dim genderAttribute = reader.GetAttribute("GUID")
+            reader.Read()
+
+            Casual = reader.Value
+            Guid = Casual
+
+        End While
+        reader.Close()
+
+
+        'Procedimiento Para Conocer el Numero de Contratos Procesar
+        Dim NumeroContratos As String
+        Connect1.NumContratos(Guid)
+
+        NumeroContratos = Connect1.ds.Tables(0).Rows(0)(0).ToString()
+
+        Connect1.con.Close()
+        Connect1.ds.Dispose()
+
+        'Validar Si Existe un Contrato Para Procesar
+        If NumeroContratos = 0 Then
+
+            Dim EsSuper As String
+            Connect5.DatosSuper(Guid)
+
+            EsSuper = Connect5.ds.Tables(0).Rows(0)(0).ToString()
+
+            If EsSuper = "SUPER INTER" Then
+
+                Process.Start("\\296imb02\WF Casual Leasing\Modelos del Proceso\Calculo Tarifa Casual Leasing Super Inter.xlsm")
+                Exit Sub
+
+            Else
+
+                Dim EsSMX As String
+                Connect6.DatosSMX(Guid)
+
+                EsSMX = Connect6.ds.Tables(0).Rows(0)(0).ToString()
+
+                If EsSMX = "Surtimax" Or EsSMX = "Bodega Surtimax" Or EsSMX = "Bodeguita Surtimax" Then
+
+                    Process.Start("\\296imb02\WF Casual Leasing\Modelos del Proceso\Calculo Tarifa Casual Leasing Surtimax.xlsm")
+                    Exit Sub
+
+                Else
+
+                    Process.Start("\\296imb02\WF Casual Leasing\Modelos del Proceso\Calculo Tarifa Casual Leasing v1.xlsm")
+                    Exit Sub
+
+                End If
+
+
+            End If
+
+        Else
+
+
+            Dim i As Integer
+
+
+
+            For i = 1 To NumeroContratos
+
+                'Consulta de Datos a Servicio Web
+
+Bifurc:
+
+                Dim Depben, Deprec, Tipocon, Nit,
+                    Nombre1, Nombre2, Apell1, Apell2, Dir,
+                    Telef, Email, Fecini, Fefin, Tiponit,
+                    Bukrs, Pais, Region, ContratoNum, Valor, Ciudad As String
+
+
+                Connect2.DatosSap(Guid)
+
+                Depben = Connect2.ds.Tables(0).Rows(0)(0).ToString()
+                Deprec = Connect2.ds.Tables(0).Rows(0)(1).ToString()
+                ContratoNum = Connect2.ds.Tables(0).Rows(0)(2).ToString()
+                Tipocon = Connect2.ds.Tables(0).Rows(0)(3).ToString()
+                Nit = Connect2.ds.Tables(0).Rows(0)(4).ToString()
+                Nombre1 = Connect2.ds.Tables(0).Rows(0)(5).ToString()
+                Nombre2 = Connect2.ds.Tables(0).Rows(0)(6).ToString()
+                Apell1 = Connect2.ds.Tables(0).Rows(0)(7).ToString()
+                Apell2 = Connect2.ds.Tables(0).Rows(0)(8).ToString()
+                Dir = Connect2.ds.Tables(0).Rows(0)(9).ToString()
+                Telef = Connect2.ds.Tables(0).Rows(0)(10).ToString()
+                Email = Connect2.ds.Tables(0).Rows(0)(11).ToString()
+                Fecini = Connect2.ds.Tables(0).Rows(0)(12).ToString()
+                Fefin = Connect2.ds.Tables(0).Rows(0)(13).ToString()
+                Valor = Connect2.ds.Tables(0).Rows(0)(14).ToString()
+                Ciudad = Connect2.ds.Tables(0).Rows(0)(15).ToString()
+                Tiponit = Connect2.ds.Tables(0).Rows(0)(16).ToString()
+                Bukrs = Connect2.ds.Tables(0).Rows(0)(17).ToString()
+                Pais = Connect2.ds.Tables(0).Rows(0)(18).ToString()
+                Region = Connect2.ds.Tables(0).Rows(0)(19).ToString()
+
+                Connect2.con.Close()
+                Connect2.ds.Dispose()
+
+                'Proceso Dependencia 2032
+                If Deprec = "2032" Then
+
+                    'Condición que evalua la función actualizar
+                    If Connect3.Dep2032(Guid, ContratoNum) Then
+                        'Exit Sub
+
+                        i = i + 1
+                        GoTo Bifurc
+
+                    End If
+
+                Else
+
+                    Dim ws = New ZFICL_CONTRATO()
+                    ws.Credentials = New System.Net.NetworkCredential("conciliador", "visualsaldo123")
+
+                    Dim contrato = New ZficlContrato()
+
+                    Dim data = New ZficlDatos()
+
+                    data.Depben = Depben
+                    data.Deprec = Deprec
+                    data.Contrato = ContratoNum
+                    data.Tipocon = Tipocon
+                    data.Nit = Nit
+                    data.Nombre1 = Nombre1
+                    data.Nombre2 = Nombre2
+                    data.Apell1 = Apell1
+                    data.Apell2 = Apell2
+                    data.Dir = Dir
+                    data.Telef = Telef
+                    data.Email = Email
+                    data.Fecini = Fecini
+                    data.Fefin = Fefin
+                    data.ValorSiniva = Valor
+                    data.Ciudad = Ciudad
+                    data.Tiponit = Tiponit
+                    data.Bukrs = Bukrs
+                    data.Pais = Pais
+                    data.Region = Region
+
+                    contrato.ItDatos = New ZficlDatos() {data}
+                    contrato.ItReturn = New Bapireturn1() {}
+
+                    Dim cadena As String
+
+                    Try
+                        Dim response = ws.ZficlContrato(contrato)
+
+                        For Each resp In response.ItReturn
+                            Console.WriteLine("{0} - {1}", resp.Type, resp.Message)
+                            Dim v1 As String
+                            v1 = resp.Message
+                            cadena = v1
+
+                            If Connect4.ActualizarFinal(cadena, Guid, ContratoNum, Depben, Deprec, ContratoNum, Tipocon, Nit, Nombre1, Nombre2, Apell1, Apell2,
+                                                Dir, Telef, Email, Fecini, Fefin, Valor, Ciudad, Tiponit, Bukrs, Pais, Region) Then
+
+                                MsgBox("El Contrato " & ContratoNum & " Se Ha Enviado a SAP Correctamente, Presione OK para continuar con el proceso.")
+
+                            End If
+
+                        Next
+
+                    Catch ex As Exception
+                        Console.WriteLine("{0}\n{1}", ex.Message, ex.StackTrace)
+
+                        MsgBox("Existe un Problema de Conexión con el Servidor de SAP, por favor repita el proceso o comuniquese con los responsables del sistema.")
+                        Exit Sub
+
+                    End Try
+
+                    'Console.ReadLine()
+
+
+                End If
+
+            Next i
+
+        End If
+
+
+    End Sub
+
+
+End Module
